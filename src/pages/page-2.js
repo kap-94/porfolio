a16989f53e2898cf9e8040435eeabefbf13d15a3
kap-94 <karamjp7@gmail.com>
2022-02-28 23:03:00 -0600

    projects order changed

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,10 +6,10 @@ import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <Seo title="Page two" />
+    {/* <Seo title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Go back to the homepage</Link> */}
   </Layout>
 )
 
